22e962b450673414c0b328e882e7c1c9afccf26e
Neil Uri Nowgaonkar <56089257+NeilNowgaonkar@users.noreply.github.com>
2022-05-05 15:32:59 +0530

    Add files via upload

--- RLE.java
+++ RLE.java
@@ -1,6 +1,9 @@
 package DAA_CP;
 
-import javax.swing.JFrame;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 class RLE
 {
@@ -37,15 +40,74 @@ class RLE
         return encoding;
     }
 
-    public static void main(String[] args)
+    public static String decode(String str)
     {
+        String src = "3A8B2 4C5D4";
+        StringBuilder dest = new StringBuilder();
 
-        JFrame frame = new JFrame();
+        for (int i = 1; i < src.length(); i = i + 2) {
+            char charAt = src.charAt(i);
+            int cnt = src.charAt(i - 1) - '0';
+            for (int j = 0; j < cnt; j++) {
+                dest.append(charAt);
+            }
+        }
+        return (dest.toString());
+    }
 
-        //String str = "AAABBBBBBBB  CCCCDDDDDDDDDDD";
-                String str = "AAAABBBBBBBBBBBBBBBBBBB  CCCCDDDDDDDDDDD";
+    public static String readFileAsString(String fileName)throws Exception
+    {
+        String data = "";
+        data = new String(Files.readAllBytes(Paths.get(fileName)));
+        return data;
+    }
+
+    public static void writeFile(String fileName, String str) throws IOException
+    {
+        Path path = Paths.get(fileName);
+        byte[] strToBytes = str.getBytes();
+
+        Files.write(path, strToBytes);
+
+    }
+
+    public static long printFileSizeNIO(String fileName) {
+
+        Path path = Paths.get(fileName);
+        long bytes=0;
+        try {
+
+            // size of a file (in bytes)
+            bytes = Files.size(path);
+            System.out.println(String.format("\n%,d bytes", bytes));
+            System.out.println(String.format("%,d kilobytes", bytes / 1024));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return bytes;
+    }
+
+    public static void main(String[] args) throws Exception {
+        //String str = "AAABBBBBBBB  CCCCDDDDD";
+        //        String str = "AAAABBBBBBBBBBBBBBBBBBB  CCCCDDDDDDDDDDD";
+        String input_file="D:\\STUDY\\JAVA QUESTIONS\\src\\DAA_CP\\Input.txt";
+        String output_file="D:\\STUDY\\JAVA QUESTIONS\\src\\DAA_CP\\RLE_Output.txt";
+
+        String str = readFileAsString(input_file);
+        String enocded_str=encode(str);
         System.out.print("\nInput String = "+str);
-        System.out.print("\nEncoded String = "+encode(str));
+        System.out.print("\nEncoded String = "+enocded_str);
+        writeFile(output_file,enocded_str);
+        long input_file_size = printFileSizeNIO(input_file);
+        long output_file_size = printFileSizeNIO(output_file);
+
+
+        float compression_percentage = (1 - (output_file_size/(float)input_file_size))*100;
+
+        System.out.println("\nCompression Percentage = "+compression_percentage+" % ");
+
         System.out.println("\n\n");
     }
 }
